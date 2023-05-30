18ccf5bfba84f14f96d81a327169e45e13119aad
At Tafani Fillah <63786515+lzddb@users.noreply.github.com>
2023-05-30 20:04:37 +0700

    Fix cek_login.php

--- admin/cek_login.php
+++ admin/cek_login.php
@@ -1,19 +1,19 @@
 <?php
 include "../config/koneksi.php";
-$pass=md5($_POST[password]);
-$login=mysqli_query("SELECT * FROM user
+$pass=md5($_POST['password']);
+$login=mysqli_query($con, "SELECT * FROM user
 WHERE id_user='$_POST[id_user]' AND password='$pass'");
-$ketemu=mysql_num_rows($login);
-$r=mysql_fetch_array($login);
+$ketemu=mysqli_num_rows($login);
+$r=mysqli_fetch_array($login);
 // Apabila username dan password ditemukan (valid)
 if ($ketemu > 0){
 session_start(); // Untuk memulai session
 // Daftarkan session ke server
-session_register("namauser");
-session_register("passuser");
+session_register('namauser');
+session_register('passuser');
 // isi dari variabel session
-$_SESSION[namauser]=$r[id_user];
-$_SESSION[passuser]=$r[password];
+$_SESSION['namauser']=$r['id_user'];
+$_SESSION['passuser']=$r['password'];
 header('location:form_berita.php'); // Buka hal input berita
 }
 else{
