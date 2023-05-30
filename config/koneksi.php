2e504fe35b40b6520d7756e27e4f0e6aa90bd1db
At Tafani Fillah <63786515+lzddb@users.noreply.github.com>
2023-05-30 18:34:46 +0700

    Update koneksi database

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,5 +1,9 @@
 <?php
-$connect_error = 'Sorry, we\'re experiencing connection issues.';
-$con = mysqli_connect('localhost', 'root', '');
-mysqli_select_db($con, 'cms') or die($connect_error);
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "cms";
+// Menghubungkan database
+$con = mysqli_connect($server,$username,$password) or die("Koneksi gagal");
+mysqli_select_db($con,$database) or die("Database tidak bisa dibuka");
 ?>
