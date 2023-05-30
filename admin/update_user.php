163adf65e7b6887b92d27a6174cae3a709a35046
lzddb <attafanif@gmail.com>
2023-05-30 21:39:32 +0700

    Perbaiki CRUD Pengguna

--- admin/update_user.php
+++ admin/update_user.php
@@ -1,21 +1,32 @@
 <?php
 include "../config/koneksi.php";
-if (empty($_POST[password])) {
- mysql_query("UPDATE users SET username = '$_POST[id_user]',
- nama_lengkap = '$_POST[nama_lengkap]',
- email = '$_POST[email]',
 
- WHERE username = '$_POST[id]'");
- }
- // Apabila password diubah
- else{
- $pass=md5($_POST[password]);
- mysql_query("UPDATE users SET username = '$_POST[username]',
- password = '$pass',
- nama_lengkap = '$_POST[nama_lengkap]',
- email = '$_POST[email]',
+if (empty($_POST['password'])) {
+    $id = $_POST['id'];
+    $id_user = $_POST['username'];
+    $nama_lengkap = $_POST['nama_lengkap'];
+    $email = $_POST['email'];
 
- WHERE username = '$_POST[id]'");
- }
- header('location:tampil_user.php');
+    $query = "UPDATE user SET id_user = '$id_user', nama_lengkap = '$nama_lengkap', email = '$email' WHERE id_user = '$id'";
+    $result = $con->query($query);
+
+    if (!$result) {
+        die("Error in query: " . $con->error);
+    }
+} else {
+    $id = $_POST['id'];
+    $id_user = $_POST['username'];
+    $password = md5($_POST['password']);
+    $nama_lengkap = $_POST['nama_lengkap'];
+    $email = $_POST['email'];
+
+    $query = "UPDATE user SET id_user = '$id_user', password = '$password', nama_lengkap = '$nama_lengkap', email = '$email' WHERE id_user = '$id'";
+    $result = $con->query($query);
+
+    if (!$result) {
+        die("Error in query: " . $con->error);
+    }
+}
+
+header('location: tampil_user.php');
 ?>
