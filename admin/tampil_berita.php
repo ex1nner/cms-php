8efaabd0054f1687efdb11f601538368fcf5e4bb
At Tafani Fillah <63786515+lzddb@users.noreply.github.com>
2023-05-30 20:30:10 +0700

    Update tampil_berita.php

--- admin/tampil_berita.php
+++ admin/tampil_berita.php
@@ -7,20 +7,20 @@ echo "<h2>Berita</h2>
 <input type=submit value='Tambah Berita'>
 </form>
 <table>
-<tr><th>no</th><th>judul</th><th>tgl. posting</th>
-<th>aksi</th></th></tr>";
-$tampil=mysql_query("SELECT * FROM berita
+<tr><th>No</th><th>Judul</th><th>Tanggal Posting</th>
+<th>Aksi</th></th></tr>";
+$tampil=mysqli_query($con, "SELECT * FROM berita
 WHERE id_user='$_SESSION[namauser]'
 ORDER BY id_berita DESC");
 $no=1;
-while ($r=mysql_fetch_array($tampil)){
-$tgl_posting=tgl_indo($r[tanggal]);
+while ($r=mysqli_fetch_array($tampil)){
+$tgl_posting=tgl_indo($r['tanggal']);
 echo "<tr><td>$no</td>
 <td>$r[judul]</td>
 <td>$tgl_posting</td>
-<td><a href=edit_berita.php?id = $r[id_berita]>
+<td><a href=edit_berita.php?id=$r[id_berita]>
 Edit</a> |
-<a href=hapus_berita.php?id = $r[id_berita]>
+<a href=hapus_berita.php?id=$r[id_berita]>
 Hapus</a></td></tr>";
 $no++;
 }
