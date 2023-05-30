c9a9c3bc8dfbc357010c5c051ddfea60d673f9d0
At Tafani Fillah <63786515+lzddb@users.noreply.github.com>
2023-05-30 19:46:21 +0700

    Fix input user

--- admin/input_user.php
+++ admin/input_user.php
@@ -1,8 +1,8 @@
 <?php
 include "../config/koneksi.php";
 //Enkripsi
- $pass=md5($_POST[password]);
-mysql_query("INSERT INTO user
+ $pass=md5($_POST['password']);
+mysqli_query($con, "INSERT INTO user
 (id_user,password,nama_lengkap,email)
 VALUES
 ('$_POST[id_user]','$pass','$_POST[nama_lengkap]','$_POST[email]')");
